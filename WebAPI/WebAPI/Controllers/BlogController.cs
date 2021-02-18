c9cb5e3344236b564afcd94fd228225ee695b6fb
unknown <harshitparikh2000@gmail.com>
2021-02-18 19:34:46 +0530

    first commit

---
+++ WebAPI/WebAPI/Controllers/BlogController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public BlogController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        // GET: api/<BlogController>
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select * from dbo.Blog";
+            DataTable table = new DataTable();
+            string dataSource = _configuration.GetConnectionString("BlogConnection");
+            SqlDataReader dataReader;
+            using (SqlConnection con = new SqlConnection(dataSource))
+            {
+                con.Open();
+                using (SqlCommand command = new SqlCommand(query, con))
+                {
+                    dataReader = command.ExecuteReader();
+                    table.Load(dataReader);
+
+                    dataReader.Close();
+                    con.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        // POST api/<BlogController>
+        [HttpPost]
+        public JsonResult Post(Blog data)
+        {
+            string query = @"insert into dbo.Blog (BlogTitle,BlogDescription,BlogImageName) values
+                ('" + data.Title + "','" + data.Description + "','" + data.Image + "')";
+
+            DataTable table = new DataTable();
+            string dataSource = _configuration.GetConnectionString("BlogConnection");
+            SqlDataReader dataReader;
+            using (SqlConnection con = new SqlConnection(dataSource))
+            {
+                con.Open();
+                using (SqlCommand command = new SqlCommand(query, con))
+                {
+                    dataReader = command.ExecuteReader();
+                    table.Load(dataReader);
+
+                    dataReader.Close();
+                    con.Close();
+                }
+            }
+            return new JsonResult("1 Row Added");
+        }
+
+        // PUT api/<BlogController>/5
+        [HttpPut("{id}")]
+        public JsonResult Put(int id, Blog data)
+        {
+            string query = @"update dbo.Blog set
+                BlogTitle = '" + data.Title + "'," +
+                "BlogDescription = '" + data.Description + "'," +
+                "BlogImageName = '" + data.Image + "'";
+
+            DataTable table = new DataTable();
+            string dataSource = _configuration.GetConnectionString("BlogConnection");
+            SqlDataReader dataReader;
+            using (SqlConnection con = new SqlConnection(dataSource))
+            {
+                con.Open();
+                using (SqlCommand command = new SqlCommand(query, con))
+                {
+                    dataReader = command.ExecuteReader();
+                    table.Load(dataReader);
+
+                    dataReader.Close();
+                    con.Close();
+                }
+            }
+            return new JsonResult("1 Row Updated");
+        }
+
+        // DELETE api/<BlogController>/5
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+             string query = @"delete from dbo.Blog where
+                BlogId = '" + id + "'";
+
+            DataTable table = new DataTable();
+            string dataSource = _configuration.GetConnectionString("BlogConnection");
+            SqlDataReader dataReader;
+            using (SqlConnection con = new SqlConnection(dataSource))
+            {
+                con.Open();
+                using (SqlCommand command = new SqlCommand(query, con))
+                {
+                    dataReader = command.ExecuteReader();
+                    table.Load(dataReader);
+
+                    dataReader.Close();
+                    con.Close();
+                }
+            }
+            return new JsonResult("1 Row Deleted");
+        }
+    }
+}
